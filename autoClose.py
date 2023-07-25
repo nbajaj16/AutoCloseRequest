2d372fbbb8a4ea8f576a779bbb3d891980b7ddbf
nbajaj16 <140163819+nbajaj16@users.noreply.github.com>
2023-07-25 12:05:54 +0530

    Update autoClose.py

--- autoClose.py
+++ autoClose.py
@@ -29,7 +29,7 @@ def close_request(number):
         "Authorization" : f"Bearer {access_token}"  # Replace with your GitHub access token
     }
     patch_request = requests.patch(patch_api, headers=headers, json=data)
-    return patch_request.json()
+    return patch_request
 
 # Check if the difference is more than 8 days for each open pull request
 for data in api_call:
@@ -37,7 +37,7 @@ for data in api_call:
         created_at_date = pd.to_datetime(data['created_at']).date()
         difference = datetime.now().date() - created_at_date
         if difference == timedelta(days=1):
-            close_request(data['number'])
-            print("Request Closed successfuly!")
+            print(close_request(data['number']))
+            # print("Request Closed successfuly!")
         elif difference > timedelta(days=8) and difference < timedelta(days=10):
             email_func()
