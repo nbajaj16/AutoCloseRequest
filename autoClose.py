eee66ee223f44b33817fa41f6ae291ec11dcf349
nbajaj16 <140163819+nbajaj16@users.noreply.github.com>
2023-07-25 12:01:35 +0530

    Update autoClose.py

--- autoClose.py
+++ autoClose.py
@@ -5,44 +5,40 @@ from datetime import datetime
 import pandas as pd
 from datetime import timedelta
 
-current_file_path = os.path.realpath(__file__)
-current_file_dir = os.path.basename(current_file_path).replace(".py", "")
+repo_owner, repo_name = "nbajaj16", "AutoCloseRequest"
 
-print(current_file_path)
+api = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls"
+api_for_email = f"https://api.github.com/users/{repo_owner}/events/public"
+access_token = os.environ["access_token"]
 
-# repo_owner, repo_name = current_file_dir.split(os.path.sep)[-3:-1]
+def get_data(api):
+    response = requests.get(api)
+    return response.json()
 
-# api = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls"
-# api_for_email = f"https://api.github.com/users/{repo_owner}/events/public"
-# access_token = os.environ["access_token"]
+api_call = get_data(api)
 
-# def get_data(api):
-#     response = requests.get(api)
-#     return response.json()
+def email_func():
+    return {}
 
-# api_call = get_data(api)
+def close_request(number):
+    patch_api = f"{api}/{number}"
+    data = {
+        'state' : 'closed'
+    } 
+    headers = {
+        "Authorization" : f"Bearer {access_token}"  # Replace with your GitHub access token
+    }
+    patch_request = requests.patch(patch_api, headers=headers, json=data)
+    return patch_request.json()
 
-# def email_func():
-#     return {}
-
-# def close_request(number):
-#     patch_api = f"{api}/{number}"
-#     data = {
-#         'state' : 'closed'
-#     } 
-#     headers = {
-#         "Authorization" : f"Bearer {access_token}"  # Replace with your GitHub access token
-#     }
-#     patch_request = requests.patch(patch_api, headers=headers, json=data)
-#     return patch_request.json()
-
-# # Check if the difference is more than 8 days for each open pull request
-# for data in api_call:
-#     if data['state'] == 'open':
-#         created_at_date = pd.to_datetime(data['created_at']).date()
-#         difference = datetime.now().date() - created_at_date
-#         if difference > timedelta(days=10):
-#             close_request(data['number'])
-#             print("Request Closed successfuly!")
-#         elif difference > timedelta(days=8) and difference < timedelta(days=10):
-#             email_func()
+# Check if the difference is more than 8 days for each open pull request
+for data in api_call:
+    if data['state'] == 'open':
+        created_at_date = pd.to_datetime(data['created_at']).date()
+        difference = datetime.now().date() - created_at_date
+        print(difference)
+        if difference > timedelta(days=10):
+            close_request(data['number'])
+            print("Request Closed successfuly!")
+        elif difference > timedelta(days=8) and difference < timedelta(days=10):
+            email_func()
